b05b357010d019365447fca6339a6c6a9125fb69
정동하 <ha4219@naver.com>
2021-08-25 00:46:12 +0900

    second commit

---
+++ graphql/db.js
@@ -0,0 +1,49 @@
+let movies = [
+    {
+        id: 0,
+        name: "1",
+        score: 1
+    },
+    {
+        id: 1,
+        name: "2",
+        score: 2
+    },
+    {
+        id: 2,
+        name: "3",
+        score: 3
+    },
+    {
+        id: 3,
+        name: "4",
+        score: 4
+    },
+    {
+        id: 4,
+        name: "5",
+        score: 5
+    },
+    {
+        id: 5,
+        name: "6",
+        score: 6
+    }
+];
+
+export const getId = id => {
+    const filteredPeople = peope.filter(person=>person.id === string(id));
+    return filteredPeople[0];
+};
+
+export const getMovies = () => movies;
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== string(id));
+    if(movies.length>cleanedMovies.length){
+        movie = cleanedMovies;
+        return true;
+    }else{
+        return false;
+    }
+}
