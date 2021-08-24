8d881e0f825db15b5c953a7e32dd1bd856504e38
정동하 <ha4219@naver.com>
2021-08-25 01:09:28 +0900

    5th commit

--- graphql/db.js
+++ graphql/db.js
@@ -1,59 +1,7 @@
-let movies = [
-    {
-        id: 0,
-        name: "1",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "2",
-        score: 2
-    },
-    {
-        id: 2,
-        name: "3",
-        score: 3
-    },
-    {
-        id: 3,
-        name: "4",
-        score: 4
-    },
-    {
-        id: 4,
-        name: "5",
-        score: 5
-    },
-    {
-        id: 5,
-        name: "6",
-        score: 6
-    }
-];
+import fetch from "node-fetch"
 
-export const getId = id => {
-    const fliteredMovies = movies.filter(movie => movie.id===id);
-    return fliteredMovies[0];
-};
+const API_URL = "https://yts.am/api/v2/list_movies.json"
 
-export const getMovies = () => movies;
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length>cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    }else{
-        return false;
-    }
-}
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    }
-    movies.push(newMovie)
-    return newMovie;
+export const getMovies = (limit, rating) => {
+    fetch(`${API_URL}`).then(res => res.json()).then(json => json.data.movies);
 }
